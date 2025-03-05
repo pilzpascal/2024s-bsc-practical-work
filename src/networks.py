bf5d56fe6e9d3317caf1f9fc1ec2ca8e657243f5
Pascal Pilz <pasc.pilz@gmail.com>
2025-03-05 15:50:46 +0100

    - added ConvNN from (Gal et al., 2017)

--- src/networks.py
+++ src/networks.py
@@ -10,11 +10,11 @@ class LeNet(nn.Module):
         super(LeNet, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, 5, padding='same')
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # images are 5x5 with 16 channels
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # images are 5x5 with 16 channels after conv and pool
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
-    def forward(self, x, use_dropout=False):                # (N, 28, 28), where N is batch size
+    def forward(self, x: torch.Tensor, use_dropout=False) -> torch.Tensor:  # (N, 28, 28), where N is batch size
 
         x = F.relu(self.conv1(x))                           # (N, 6, 28, 28)
         x = F.max_pool2d(x, kernel_size=2)                  # (N, 6, 14, 14)
@@ -31,4 +31,28 @@ class LeNet(nn.Module):
         x = F.relu(self.fc2(x))                             # (N, 84)
 
         x = self.fc3(x)                                     # (N, 10)
+        return x
+
+
+class ConvNN(nn.Module):
+    def __init__(self):
+        super(ConvNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 4, padding='same')
+        self.conv2 = nn.Conv2d(32, 32, 4)
+        self.fc1 = nn.Linear(32 * 12 * 12, 128)  # images are 12x12 with 32 channels after conv and pool
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x: torch.Tensor, use_dropout=False) -> torch.Tensor:
+
+        x = F.relu(self.conv1(x))                           # (N, 32, 28, 28)
+        x = F.relu(self.conv2(x))                           # (N, 32, 25, 25)
+        x = F.max_pool2d(x, kernel_size=2)                  # (N, 32, 12, 12)
+
+        x = torch.flatten(x, 1)                             # (N, 32 * 12 * 12) = (N, 4608)
+
+        x = F.dropout(x, p=0.25, training=use_dropout)      # (N, 4608)
+        x = F.relu(self.fc1(x))                             # (N, 128)
+
+        x = F.dropout(x, p=0.5, training=use_dropout)       # (N, 128)
+        x = self.fc2(x)                                     # (N, 10)
         return x
